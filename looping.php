e07c539978c786976d73baa045ab3f86eeffcf9d
NurulHidayati05 <libertyrull73@gmail.com>
2024-12-18 11:18:31 +0700

    Add files via upload

---
+++ looping.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Looping menggunakan For : </br>";
+for ($i = 1; $i <= 10 ; $i++) {
+    echo "<li> Urutan ke-$i</li>";
+}
+
+echo "</br>";
+echo "Looping mrnggunakan While";
+$i = 1;
+While ($i <= 10) {
+    echo "<li> Urutan ke-$i</li>";
+    $i++;
+}
+
+?>
